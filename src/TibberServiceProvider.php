99926d952d2a54119b6bb7a3763c088cbaec9af4
Arne Breitsprecher <arnebr@gmail.com>
2023-02-10 16:16:50 +0000

    Adding notification

--- src/TibberServiceProvider.php
+++ src/TibberServiceProvider.php
@@ -3,9 +3,10 @@
 namespace Arnebr\Tibber;
 
 use Arnebr\Tibber\Commands\TibberCommand;
+use Illuminate\Notifications\ChannelManager;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-
+use Illuminate\Support\Facades\Notification;
 class TibberServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
@@ -21,5 +22,11 @@ class TibberServiceProvider extends PackageServiceProvider
             //->hasViews()
             //->hasMigration('create_laravel-tibber_table')
             ->hasCommand(TibberCommand::class);
+
+            Notification::resolved(function (ChannelManager $service) {
+                $service->extend('tibber', function ($app) {
+                    return new Channels\TibberChannel();
+                });
+            });
     }
 }
